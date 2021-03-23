9ee31265f92f5ce0e891abe17032d8bbff3ae265
Alex Terenda <alexterenda@gmail.com>
2021-03-23 14:39:51 +0000

    Changed mock data location, updated imports, built App component

--- react-airbnb/src/App.js
+++ react-airbnb/src/App.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+import flats from "./data/data";
+import FlatList from "./components/flat_list";
+import Marker from "./components/marker";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats,
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng,
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  };
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker
+              lat={this.state.selectedFlat.lat}
+              lng={this.state.selectedFlat.lng}
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
