a817b55cb1b11a04eb937aed08bae21061c12794
Alex Terenda <alexterenda@gmail.com>
2021-03-23 08:52:05 +0000

    Remove file config

--- react-giphy/src/App.js
+++ react-giphy/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 import SearchBar from "./components/search_bar";
 import Gif from "./components/gif";
+import GifList from "./components/gif_list";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "RoFXqXWN639Qs",
+    };
+  }
+
   render() {
+    const gifs = [
+      {
+        id: "RoFXqXWN639Qs",
+      },
+      {
+        id: "RoFXqXWN639Qs",
+      },
+    ];
     return (
       <div className="App">
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="RoFXqXWN639Qs" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
