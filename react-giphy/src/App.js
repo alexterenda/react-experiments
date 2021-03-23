8871887cb159b5b9b6e4fe0f676acd7e01c970d2
Alex Terenda <alexterenda@gmail.com>
2021-03-23 08:05:37 +0000

    Search and gif components

--- react-giphy/src/App.js
+++ react-giphy/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import SearchBar from "./components/search_bar";
+import Gif from "./components/gif";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif />
+          </div>
+        </div>
+        <div className="right-scene"></div>
       </div>
     );
   }
---
+++ react-giphy/src/components/gif.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Gif extends Component {
+  render() {
+    return <p className="Hi"></p>;
+  }
+}
+
+export default Gif;
