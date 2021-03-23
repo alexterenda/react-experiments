7b934171d91555267547ddca70538888c0fb2950
Alex Terenda <alexterenda@gmail.com>
2021-03-23 08:34:26 +0000

    Added gif_list component

---
+++ react-giphy/src/components/gif_list.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Gif from "./gif";
+
+const GifList = (props) => {
+  return (
+    <div className="gif-list">
+      {props.gifs.map((gif) => (
+        <Gif id={gif.id} key={gif.id} />
+      ))}
+    </div>
+  );
+};
+
+export default GifList;
