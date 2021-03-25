2b0d8ba9ffe63cc0ae55bc3639858a2015b6ce9d
Alex Terenda <alexterenda@gmail.com>
2021-03-25 08:25:42 +0000

    Container imports

--- redux-cities/src/App.js
+++ redux-cities/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import CityList from "./containers/city_list";
+import ActiveCity from "./containers/active_city";
 
 const cities = [
   { name: "Paris", address: "16 Villa Gaudelet, 75011 Paris", slug: "paris" },
@@ -16,7 +17,7 @@ const App = () => {
   return (
     <div className="app">
       <CityList cities={cities} />
-      {/* <ActiveCity /> */}
+      <ActiveCity />
     </div>
   );
 };
