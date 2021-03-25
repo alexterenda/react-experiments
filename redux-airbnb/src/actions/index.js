6abbd216b4ebee7d805f4acfef9dd609dfa02899
Alex Terenda <alexterenda@gmail.com>
2021-03-25 09:45:07 +0000

    Fetch flats

--- redux-airbnb/src/actions/index.js
+++ redux-airbnb/src/actions/index.js
@@ -1,10 +1,16 @@
 import flats from "../flats";
 
 export function setFlats() {
-  return {
-    type: "SET_FLATS",
-    payload: flats,
-  };
+  return fetch(
+    "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return {
+        type: "SET_FLATS",
+        payload: data,
+      };
+    });
 }
 
 export function selectFlat(flat) {
