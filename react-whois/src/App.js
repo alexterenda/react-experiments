51394149cda45e0901a4fa8dc174d4ab8cf1a69b
Alex Terenda <alexterenda@gmail.com>
2021-04-15 04:52:55 +0100

    Add controlled input form

--- react-whois/src/App.js
+++ react-whois/src/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [domain, setDomain] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(domain);
+  };
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='domain'>Domain : </label>
+          <input
+            type='text'
+            id='domain'
+            name='domain'
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
