7f0d152de3979413dd970538e4b65f63e7c56cf5
Alex Terenda <alexterenda@gmail.com>
2021-04-15 06:26:43 +0100

    Fixed results map bug, fixed domainData initialisation bug

--- react-whois/src/App.js
+++ react-whois/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const fetchData = async () => {
     const response = await fetch(url);
     const responseObject = await response.json();
-    setDomainData(responseObject);
+    setDomainData(responseObject['data']);
     console.log(domainData);
   };
 
@@ -42,6 +42,26 @@ const App = () => {
         </div>
         <button type='submit'>Submit</button>
       </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Record</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Only using index as key to remove error and no CRUD operations involved */}
+
+          {domainData.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item['name']}</td>
+                <td>{item['value']}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
