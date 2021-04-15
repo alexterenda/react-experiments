8a66a644cd71a4384106290ef20453cfed07a5ec
Alex Terenda <alexterenda@gmail.com>
2021-04-15 05:09:45 +0100

    Fetch data using domain value

--- react-whois/src/App.js
+++ react-whois/src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 const App = () => {
   const [domain, setDomain] = useState('');
 
+  const url = `https://otx.alienvault.com/otxapi/indicator/domain/whois/${domain}`;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(domain);
+    fetchInfo();
   };
+
+  const fetchInfo = async () => {
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   return (
     <div>
